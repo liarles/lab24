92296f8f243a960f2e6415a5eb83329b1ca8fbfa
liarles <62631264+liarles@users.noreply.github.com>
2025-03-06 12:21:01 +0700

    Update lab24_3.cpp

--- lab24_3.cpp
+++ lab24_3.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
-//Write function count() here.
+int count(int arr[], int size) {
+    set<int> unique_numbers; // ใช้ set เพื่อเก็บค่าที่ไม่ซ้ำกัน
 
+    for (int i = 0; i < size; i++) {
+        unique_numbers.insert(arr[i]); // ใส่ค่าลงใน set
+    }
+
+    return unique_numbers.size(); // คืนค่าจำนวนสมาชิกที่ไม่ซ้ำกัน
+}
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
 	
